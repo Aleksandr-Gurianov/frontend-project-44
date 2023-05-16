70d245a7c3677c5198dbbe96a0b5a2a095d9aeed
Alexander Gyrianov <gyryanovalexander@gmail.com>
2023-05-16 12:27:54 -0300

    Update: code for import

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,8 @@
 import readlineSync from 'readline-sync';
 
+export const greeting = ()=>{
   console.log('Welcome to the Brain Games!');
-  export const name = readlineSync.question('May I have your name? ');
+  const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
+  return name;
+};
