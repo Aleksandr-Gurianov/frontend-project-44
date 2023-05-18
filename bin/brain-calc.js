7253fe4d722760bfa61abdde7935604f0dd60a4f
Alexander Gyrianov <gyryanovalexander@gmail.com>
2023-05-18 06:57:17 -0300

    update

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
 import calcgame from '../src/games/brain-calc-definition.js';
 
-calcgame;
+calcgame
--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
 import playprime from '../src/games/brain-prime-definition.js';
 
-playprime;
+playprime
