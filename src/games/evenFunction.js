09c0acc1e3ba6df850ca9a37710cf55c8b7a6afb
Alexander Gyrianov <gyryanovalexander@gmail.com>
2023-05-19 06:34:15 -0300

    Update code

--- src/games/evenFunction.js
+++ src/games/evenFunction.js
@@ -3,11 +3,16 @@ import randomRange from '../utils.js';
 
 const textofEven = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-const gameEven = () => {
-  const number = randomRange();
+const understandEvenorOdd = (number) => {
   let numOddEv = '';
   if (number % 2 === 0) { numOddEv = 'yes'; } else { numOddEv = 'no'; }
   return [number, numOddEv];
+};
+
+const gameEven = () => {
+  const number = randomRange();
+  const result = understandEvenorOdd(number);
+  return result;
 };
 
 const evenplay = playNewGame(textofEven, gameEven);
